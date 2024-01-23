a7d17264b12c1ef14ec0c6b00823161d3df39919
Muhammad Mahfud <laililmahvut@gmail.com>
2024-01-23 15:38:57 +0700

    kernel scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $this->runJob($schedule,'07:00');  //Run the task every day at 07:00
+        $this->runJob($schedule,'10:00');  //Run the task every day at 10:00
     }
 
     /**
@@ -21,5 +23,17 @@ class Kernel extends ConsoleKernel
     protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
+    }
+
+    private function runJob(Schedule $schedule,$time){
+        $schedule->command('use-case-2:insert-company-dashboard-data')->dailyAt($time); 
+        $schedule->command('use-case-2:insert-asset-dashboard-data')->dailyAt($time); 
+        $schedule->command('use-case-2:insert-block-dashboard-data')->dailyAt($time); 
+        $schedule->command('use-case-2:insert-field-dashboard-data')->dailyAt($time); 
+
+
+        $schedule->command('use-case-2:insert-company-chart-data')->dailyAt($time); 
+        $schedule->command('use-case-2:insert-asset-chart-data')->dailyAt($time); 
+        $schedule->command('use-case-2:insert-block-chart-data')->dailyAt($time); 
     }
 }
