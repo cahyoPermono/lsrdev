11b50747bb6d0c9dc48f96286c22d577bdd63764
Muhammad Mahfud <laililmahvut@gmail.com>
2024-01-29 09:14:22 +0700

    hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $this->runJob($schedule,'07:00');  //Run the task every day at 07:00
-        $this->runJob($schedule,'10:00');  //Run the task every day at 10:00
+        $this->runJob($schedule); 
     }
 
     /**
@@ -25,15 +24,15 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
     }
 
-    private function runJob(Schedule $schedule,$time){
-        $schedule->command('use-case-2:insert-company-dashboard-data')->dailyAt($time); 
-        $schedule->command('use-case-2:insert-asset-dashboard-data')->dailyAt($time); 
-        $schedule->command('use-case-2:insert-block-dashboard-data')->dailyAt($time); 
-        $schedule->command('use-case-2:insert-field-dashboard-data')->dailyAt($time); 
+    private function runJob(Schedule $schedule){
+        $schedule->command('use-case-2:insert-company-dashboard-data')->hourly(); 
+        $schedule->command('use-case-2:insert-asset-dashboard-data')->hourly(); 
+        $schedule->command('use-case-2:insert-block-dashboard-data')->hourly(); 
+        $schedule->command('use-case-2:insert-field-dashboard-data')->hourly(); 
 
 
-        $schedule->command('use-case-2:insert-company-chart-data')->dailyAt($time); 
-        $schedule->command('use-case-2:insert-asset-chart-data')->dailyAt($time); 
-        $schedule->command('use-case-2:insert-block-chart-data')->dailyAt($time); 
+        $schedule->command('use-case-2:insert-company-chart-data')->hourly(); 
+        $schedule->command('use-case-2:insert-asset-chart-data')->hourly(); 
+        $schedule->command('use-case-2:insert-block-chart-data')->hourly(); 
     }
 }
