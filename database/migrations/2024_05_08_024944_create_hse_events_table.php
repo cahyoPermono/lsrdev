204fa5c512555155570771285669dd4a4fc746f7
laililmahfud <laililmahvut@gmail.com>
2024-05-08 09:59:08 +0700

    hse database migration

---
+++ database/migrations/2024_05_08_024944_create_hse_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hse_events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description');
+            $table->string('category');
+            $table->longText('file');
+            $table->text('participant')->nullable();
+            $table->dateTime('start_at');
+            $table->dateTime('end_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hse_events');
+    }
+};
