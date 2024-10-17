41f5dc038a241c5da7af6ae356b97430a423801a
laililmahfud <laililmahvut@gmail.com>
2024-10-17 10:59:16 +0700

    feat : change mandatory to string

--- app/Services/MedcoApi/TrainingService.php
+++ app/Services/MedcoApi/TrainingService.php
@@ -84,7 +84,7 @@ class TrainingService
                "requirement_type" => @$row['course_type'],
                "training_type" => @$row['course_type'],
                "last_taken" => @$row['last_training_date'] ? date('Y-m-d', strtotime(@$row['last_training_date'])) : null,
-               "mandatory" => false,
+               "mandatory" => 'No',
                "personel_type" => null,
                "detail_category" => 'one',
                "detail" => [
