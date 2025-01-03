9581ddb6c5f173e88869b00cba157c8b14ab214f
nwijan <naufal.wijanarko@sc.medcoenergi.com>
2025-01-03 09:38:32 +0700

    fix: handle invalid token signature

--- app/Http/Middleware/ApiPrivateMiddleware.php
+++ app/Http/Middleware/ApiPrivateMiddleware.php
@@ -4,13 +4,12 @@ namespace App\Http\Middleware;
 
 use App\Helpers\Optimize;
 use Closure;
+use Firebase\JWT\SignatureInvalidException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 use Laililmahfud\Adminportal\Api\Error;
 use Laililmahfud\Adminportal\Api\JwtToken;
 use Laililmahfud\Adminportal\Traits\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use \Barryvdh\Debugbar\Facades\Debugbar;
 use Firebase\JWT\ExpiredException;
 
 class ApiPrivateMiddleware
@@ -23,12 +22,8 @@ class ApiPrivateMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        try{
-            JwtToken::decode();
-        } catch (ExpiredException) {
-            return $this->unauthorized("Your token is expired !", Error::EXPIRED_TOKEN);
-        }
         try {
+            JwtToken::decode();
             $token = $request->header('authorization');
             $dataToken = Optimize::cacheRememberForever("data-token:{$token}", function () {
                 return $this->auth();
@@ -36,16 +31,24 @@ class ApiPrivateMiddleware
             $isBlacklistToken = Optimize::cacheForever("is-blacklist-token:{$token}", JwtToken::isBlacklist());
 
             if ($isBlacklistToken) {
-                return $this->unauthorized('Your token was not found !', Error::FORBIDDEN);
+                return $this->unauthorized('Your token is not found !', Error::FORBIDDEN);
             }
             if (@$dataToken->regid != $request->header('regid')) {
-                return $this->unauthorized('Your token was invalid !', Error::INVALID_TOKEN);
+                return $this->unauthorized('Your token is invalid !', Error::INVALID_TOKEN);
             }
+            
 
             return $next($request);
-        } catch (\Exception $e) {
+        } catch (ExpiredException $e) {
             logger($e);
+            return $this->unauthorized("Your token is expired !", Error::EXPIRED_TOKEN);
+        } catch (SignatureInvalidException $e){
+            logger($e);
+            return $this->unauthorized('Your token is invalid !', Error::INVALID_TOKEN);
         }
-        return $this->unauthorized('Your token was not found !', Error::FORBIDDEN);
+        catch (\Exception $e) {
+            logger($e);
+            return $this->unauthorized('Your token is not found !', Error::FORBIDDEN);
+        }
     }
 }
